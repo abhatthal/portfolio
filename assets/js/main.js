04999013c2cf5fe95e0d298156b0643af62ebc62
Akash Bhatthal <akash_bhatthal@sfu.ca>
2021-01-26 09:10:39 -0800

    Section formatting and anchor scroll offset

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', function() {
     document.getElementById('load-overlay').style.opacity = 0;
 });
+
+let defaultDuration = 1200; // ms
+let edgeOffset = 110; // px
+zenscroll.setup(defaultDuration, edgeOffset);
